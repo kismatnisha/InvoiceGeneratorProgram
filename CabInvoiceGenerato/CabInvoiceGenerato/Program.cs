240016387d59380859a39f9a29b428931fdc465a
qismat <qismatnisha6243@gmail.com>
2023-01-18 17:16:14 +0530

    [qismat] Add : UC1-CalculateFare

---
+++ CabInvoiceGenerato/CabInvoiceGenerato/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CabInvoiceGenerato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+            //UC1
+            CabInvoiceGenerator cabInvoiceGeneratorApp = new CabInvoiceGenerator();
+            double fare = cabInvoiceGeneratorApp.CalculatFare(2.0, 4.0);
+            Console.WriteLine("The Total Fare For The Journey : " + fare);
+
+        }
+    }
+}
