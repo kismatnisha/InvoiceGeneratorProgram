613012e1e0bfaef8fb5d7064cecb11cfeb51bd74
qismat <qismatnisha6243@gmail.com>
2023-01-18 17:24:36 +0530

    [qismat] Add :UC2-MultipleFare

--- CabInvoiceGenerato/CabInvoiceGenerato/Program.cs
+++ CabInvoiceGenerato/CabInvoiceGenerato/Program.cs
@@ -10,6 +10,10 @@
             double fare = cabInvoiceGeneratorApp.CalculatFare(2.0, 4.0);
             Console.WriteLine("The Total Fare For The Journey : " + fare);
 
+            //UC2
+            Ride[] ride = { new Ride(1, 2), new Ride(3, 4) };
+            double getMultiple = cabInvoiceGeneratorApp.GetMultipleRideFare(ride);
+            Console.WriteLine("\nThe Aggregate Total For All : " + getMultiple);
         }
     }
 }
